2c0f899ca14810faeb831c626e8f8536ec179e43
Darya <aquarellist@mail.ru>
2021-05-18 17:50:02 +0300

    Недофрактал черепашьей графикой

---
+++ 01_06_i_dont_know_what_is_it_but_it_is_funny.py
@@ -0,0 +1,36 @@
+import turtle as tl
+
+def draw_fractal(size):
+    if size >= 4:
+        tl.pensize(max(size / 60, 1))
+        tl.left(30)
+        tl.forward(size)
+        tl.left(60)
+        tl.forward(size)
+        tl.left(120)
+        tl.forward(size)
+        tl.left(120)
+        draw_fractal(size / 1.4)
+        tl.right(60)
+        tl.penup()
+        tl.backward(size)
+        tl.pendown()
+    else:
+        tl.pensize(3)
+        
+size = 120
+
+tl.delay(1)
+tl.penup()
+tl.color('orange')
+tl.bgcolor('black')
+tl.pendown()
+
+for i in range(1,6):
+    draw_fractal(size)
+    tl.penup()
+    tl.goto(0,0)
+    tl.pendown()
+    tl.setheading(60*i)
+draw_fractal(size)
+tl.done()
